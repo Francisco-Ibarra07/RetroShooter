10ad9dea0ba64fae3177cab91e6f075c16738d07
Francisco I-XPS <Francisco.ibarra@sjsu.edu>
2019-09-24 18:51:58 -0700

    When the player presses the space bar the turret emitter spawns new sprites

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -12,6 +12,29 @@ void ofApp::setup(){
 	player1.speed = 10;
 	player1.shootKey = ' ';
 	ofSetBackgroundColor(ofColor::black);
+
+	// Load the bullet image. Exit if failure
+	if (bulletImage.load("images/space-invaders-ship-scaled.png")) {
+		bulletImageLoaded = true;
+	}
+	else {
+		ofLogFatalError("can't load image: images/space-invaders-ship-scaled.png");
+		ofExit();
+	}
+
+	// Setup turret emitter
+	turretEmitter = new Emitter(new SpriteSystem());
+	turretEmitter->setPosition(ofVec3f(player1.xPosition, player1.yPosition, 0));
+	turretEmitter->drawable = false;                // make emitter itself invisible
+	turretEmitter->setChildImage(bulletImage);
+	turretEmitter->start();
+
+	// Setup gui
+	gui.setup();
+	gui.add(rate.setup("rate", 1, 1, 10));
+	gui.add(life.setup("life", 5, .1, 10));
+	gui.add(velocity.setup("velocity", ofVec3f(0, -700, 0), ofVec3f(-1000, -1000, -1000), ofVec3f(1000, 1000, 1000)));
+	showGUI = false;
 }
 
 //--------------------------------------------------------------
@@ -33,16 +56,40 @@ void ofApp::update(){
 	else if (keyIsDown['a']) { 
 		player1.xPosition -= player1.speed;
 	}
+
+	// Update Turret Emitter variables
+	// If player presses the shoot key, then new "bullet" sprites can be spawned
+	if (keyIsDown[player1.shootKey]) {
+		turretEmitter->startSpriteCreation();
+	}
+	else {
+		turretEmitter->stopSpriteCreation();
+	}
+
+	turretEmitter->setRate(rate);
+	turretEmitter->setLifespan(life * 1000);
+	turretEmitter->setPosition(ofVec3f(player1.xPosition, player1.yPosition, 0));
+	turretEmitter->setVelocity(ofVec3f(velocity->x, velocity->y, velocity->z));
+	turretEmitter->update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
 	player1.draw();
+	turretEmitter->draw();
+
+	if (showGUI) {
+		gui.draw();
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
 	keyIsDown[key] = true;
+
+	// Show/Hide GUI
+	if (key == 'h')
+		showGUI = !showGUI;
 }
 
 //--------------------------------------------------------------
